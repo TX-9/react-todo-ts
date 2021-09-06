ccdbead963cea4ad2b8f991392dbe60e81f836cc
TX-9 <jacobleecs@gmail.com>
2021-09-06 17:40:15 -0400

    add Input

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useRef } from "react";
 
 const NewToDo = () => {
+    const todoTextInputRef = useRef<HTMLInputElement>(null);
     const submitHandler = (event: React.FormEvent) => {
         event.preventDefault();
+
     };
 
     return <form onSubmit={submitHandler}>
         <label htmlFor='text'>Todo text</label>
-        <input type='text' />
+        <input type='text' id='text' ref={todoTextInputRef} />
         <button>Add Todo</button>
     </form>
 };
